ce3070308457871f000503df42aa4d5617c4cee9
Oussama Sina <124634524+OssNipe@users.noreply.github.com>
2024-05-12 00:43:57 +0100

    wow

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,17 +1,33 @@
 import { Button } from "@/components/ui/button";
-import Link from 'next/link'
+import Link from "next/link";
+import React from "react";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage= () => {
-  return ( 
-    <div>
-      <Link href="/teacher/create">
-        <Button className="p-6">
-          New Course
-        </Button>
-      </Link>
-      
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
     </div>
-   );
-}
- 
+  );
+};
+
 export default CoursesPage;
